dbc5f01f5c43082f6ecfacffe0baa74388ac8edb
alejo <alejortizp@gmail.com>
2025-10-26 20:44:27 -0500

    adicion de funcionalidad de filtro para genero

--- src/app/genre/genre.component.ts
+++ src/app/genre/genre.component.ts
@@ -25,3 +25,39 @@ export class GenreComponent implements OnInit {
     ];
   }
 }
+
+
+import { Component, OnInit } from '@angular/core';
+import { Genre } from './genre';
+import { GenreService } from './genre.service';
+
+@Component({
+  selector: 'app-genre',
+  templateUrl: './genre.component.html',
+  styleUrls: ['./genre.component.css']
+})
+export class GenreComponent implements OnInit {
+  genres: Genre[] = [];
+  filteredGenres: Genre[] = [];
+  searchText: string = '';
+
+  constructor(private genreService: GenreService) {}
+
+  ngOnInit(): void {
+    this.getGenres();
+  }
+
+  getGenres(): void {
+    this.genreService.getGenres().subscribe(data => {
+      this.genres = data.sort((a, b) => a.name.localeCompare(b.name));
+      this.filteredGenres = [...this.genres];
+    });
+  }
+
+  filterGenres(): void {
+    const text = this.searchText.toLowerCase().trim();
+    this.filteredGenres = this.genres.filter(d =>
+      d.name.toLowerCase().includes(text)
+    );
+  }
+}
