19db6e8fd4b62f508ba257b4408a641ad107e2e8
alejo <alejortizp@gmail.com>
2025-11-06 18:07:41 -0500

    creacion de los modulos genre-list y genre-detail

---
+++ src/app/genre/genre-detail/genre-detail.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GenreDetailComponent } from './genre-detail.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [GenreDetailComponent]
+})
+export class GenreDetailModule { }
---
+++ src/app/genre/genre-list/genre-list.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GenreListComponent } from './genre-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [GenreListComponent]
+})
+export class GenreListModule { }
