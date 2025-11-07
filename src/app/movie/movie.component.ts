8f60fe832a63778e7d6f5298b8b92432116cf3ae
jarodriguezf-uni <ja.rodriguezf@uniandes.edu.co>
2025-11-06 21:26:22 -0500

    feat: Update movie component layout and styles, including search

--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { Movie } from './movie';
 import { MovieService } from './movie.service';
 
@@ -11,6 +11,9 @@ export class MovieComponent implements OnInit {
   movies: Movie[] = [];
   filteredMovies: Movie[] = [];
   searchText: string = '';
+  showSearch: boolean = false;
+
+  @ViewChild('searchWrapper', { static: false }) searchWrapper?: ElementRef;
 
   constructor(private movieService: MovieService) {}
 
@@ -30,6 +33,19 @@ export class MovieComponent implements OnInit {
     this.filteredMovies = this.movies.filter(m =>
       m.title.toLowerCase().includes(text)
     );
+  }
+
+  toggleSearch(): void {
+    this.showSearch = !this.showSearch;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: Event): void {
+    if (!this.showSearch) return;
+    const wrapperEl = this.searchWrapper?.nativeElement as HTMLElement | undefined;
+    if (wrapperEl && !wrapperEl.contains(event.target as Node)) {
+      this.showSearch = false;
+    }
   }
 }
 
